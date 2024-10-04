e88a629a4432f33fad11dfca7a938d89fd1235d4
PENTEX78 <zypherada@gmail.com>
2024-10-04 14:10:21 -0400

    Delete 4 directory

--- 4/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();  // Elimina todas las variables de sesión
-session_destroy();  // Destruye la sesión
-
-// Redirige al inicio u otra página tras cerrar sesión
-header("Location: index.php");
-exit();
-?>
--- 4/resources/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$enlace = mysqli_connect("localhost", "root", "", "bdnestor");
-if(!$enlace){
-    die("No pudo conectarse a la base de datos " . mysqli_error($mysqli));
-}
-
-?>
